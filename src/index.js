38c4af1fbf50344f211de496414508305925d1dd
Thuy <clearlythuydoan@gmail.com>
2017-10-18 17:33:08 -0400

    add japanese locale

--- src/index.js
+++ src/index.js
@@ -2,33 +2,35 @@ import React from 'react';
 import { render } from 'react-dom';
 import { IntlProvider, addLocaleData } from 'react-intl';
 import { BrowserRouter } from 'react-router-dom';
-// import { Route, Link } from 'react-router-dom';
 
 // Unless you're localizing in node, React Intl only comes with English by default.
 // We must add locale data manually.
-// If you support more languages, you can dynamically load locale data too.
+// TO-DO: How to address when you support a lot of locales?
 import en from 'react-intl/locale-data/en';
 import es from 'react-intl/locale-data/es';
 import fr from 'react-intl/locale-data/fr';
 import it from 'react-intl/locale-data/it';
+import ja from 'react-intl/locale-data/ja';
 
 import App from './App';
 
 import './index.css';
 
-addLocaleData([...en, ...es, ...fr, ...it]);
+addLocaleData([...en, ...es, ...fr, ...it, ...ja]);
 
 // Define user's language. Different browsers have the user locale defined
 // on different fields on the `navigator` object, so we make sure to account
 // for these differences by checking all of them
-const language = (navigator.languages && navigator.languages[0]) ||
-navigator.language ||
-navigator.userLanguage;
+// const language = (navigator.languages && navigator.languages[0]) ||
+// navigator.language ||
+// navigator.userLanguage;
+
+const language = "fr";
 
 render(
     <IntlProvider locale={language}>
         <BrowserRouter>
-            <App />
+            <App locale={language} />
         </BrowserRouter>
     </IntlProvider>,
     document.getElementById('root')
