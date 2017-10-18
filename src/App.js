5984d6df764f95c8b3537660999698dd746b6e69
Thuy <clearlythuydoan@gmail.com>
2017-10-18 17:32:52 -0400

    set up basic routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
-import Header from './components/Header';
+import { Route, Link } from 'react-router-dom';
+
+import Batman from './components/Batman';
+import Robin from './components/Robin';
+import LocaleInfo from './components/LocaleInfo';
+// import SelectLocale from './components/SelectLocale';
 
 import './App.css';
 
 class App extends Component {
   render() {
+        const { locale } = this.props;
     return (
         <div>
-            <Header/>
+            <h1>React Intl + React Router</h1>
+            <Link to="/">Batman</Link> | <Link to="/robin">Robin</Link>
+            <Route exact path="/" component={Batman} />
+            <Route path="/robin" component={Robin} />
+            <LocaleInfo locale={locale} />
+            {/* How can we utilize a menu to let users change their locale? */}
+            {/* <SelectLocale /> */}
         </div>
     );
   }
