b671d3963f55fe7882f5dd557c0a3649dfc3565c
Thuy <clearlythuydoan@gmail.com>
2017-10-18 17:54:00 -0400

    set up batman and robin states

--- src/components/Batman.js
+++ src/components/Batman.js
@@ -1,17 +1,33 @@
 import React, { Component } from 'react';
-import { FormattedDate } from 'react-intl';
+import { FormattedDate, FormattedTime } from 'react-intl';
 import { Link } from 'react-router-dom';
 
 // Component Source: https://github.com/yahoo/react-intl/wiki/Components
 
+// I know this component is the exact same as the Robin. 
 export default class Batman extends Component {
+    constructor() {
+        super();
+        this.state = {
+            bank: 9248123,
+            unreadCount: 5948,
+        }
+    }
     render() {
         return (
             <div>
                 <h1>Hello, Mr. Wayne.</h1>
+                <div>
+                    It is currently {' '} {/* ADDRESS: Can you use non-24 hour clock? */}
+                    <FormattedTime 
+                        value={Date.now()}
+                        hour="2-digit"
+                        minute="numeric"
+                    />.
+                </div>
                 <img 
                     src="https://i.pinimg.com/736x/e5/a0/69/e5a06942fa42823c88be5f3a834e063d--fantastic-art-bat-family.jpg"
-                    width="200" 
+                    height="250" 
                     alt="bat cave"
                 />
                 <p>Today is {' '} {/* <- This is literally just a space. */}
--- src/components/Robin.js
+++ src/components/Robin.js
@@ -1,15 +1,31 @@
 import React, { Component } from 'react';
-import { FormattedDate } from 'react-intl';
+import { FormattedDate, FormattedTime } from 'react-intl';
 import { Link } from 'react-router-dom';
 
+// I know this component is the exact same as the Batman. 
 export default class Robin extends Component {
+    constructor() {
+        super();
+        this.state = {
+            bank: 55645,
+            unreadCount: 1
+        }
+    }
     render() {
         return (
             <div>
                 <h1>Hello, Mr. Grayson.</h1>
+                <div>
+                    It is currently {' '} {/* ADDRESS: Can you use non-24 hour clock? */}
+                    <FormattedTime 
+                        value={Date.now()}
+                        hour="2-digit"
+                        minute="numeric"
+                    />.
+                </div>
                 <img 
                     src="https://i.pinimg.com/736x/bc/7a/84/bc7a84030d20079d6440a89bce798344--robin-dc-grant-morrison.jpg"
-                    width="200" 
+                    height="250" 
                     alt="bat cave"
                 />
                 <p>Today is {' '} {/* <- This is literally just a space. */}
