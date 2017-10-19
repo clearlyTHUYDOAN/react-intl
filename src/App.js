a3d6d63546fe55731af67624f3a1fd80fea00398
Thuy <clearlythuydoan@gmail.com>
2017-10-19 11:31:29 -0400

    add route for prodigy test

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Link } from 'react-router-dom';
 
 import Batman from './components/Batman';
 import Robin from './components/Robin';
+import Prodigy from './components/Prodigy';
 import LocaleInfo from './components/LocaleInfo';
 // import SelectLocale from './components/SelectLocale';
 
@@ -14,9 +15,10 @@ class App extends Component {
     return (
         <div>
             <h1>React Intl + React Router</h1>
-            <Link to="/">Batman</Link> | <Link to="/robin">Robin</Link>
+            <Link to="/">Batman</Link> | <Link to="/robin">Robin</Link> | <Link to="/prodigy">Prodigy</Link>
             <Route exact path="/" component={Batman} />
             <Route path="/robin" component={Robin} />
+            <Route path="/prodigy" component={Prodigy} />
             <LocaleInfo locale={locale} />
             {/* How can we utilize a menu to let users change their locale? */}
             {/* <SelectLocale /> */}
