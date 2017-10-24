1b235099d19e60a934ac65d796f86c7d2421b1ba
Thuy <clearlythuydoan@gmail.com>
2017-10-24 17:30:28 -0400

    map through locale list in component state

--- src/components/SelectLocale.js
+++ src/components/SelectLocale.js
@@ -1,29 +1,51 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 
 import { updateLocale } from '../actions/locales';
 
 const mapStateToProps = state => ({
-    currentLocale: state.currentLocale,
-})
-  
+    currentLocale: state.currentLocale
+});
+
 const mapDispatchToProps = {
     updateLocale
-}
+};
 
 class SelectLocale extends Component {
+    constructor() {
+        super();
+        this.state = {
+            localeList: [
+                { name: 'English', tag: 'en' },
+                { name: 'Spanish', tag: 'es' },
+                { name: 'French', tag: 'fr' },
+                { name: 'Italian', tag: 'it' },
+                { name: 'Japanese', tag: 'ja' }
+            ]
+        };
+    }
     render() {
         const { currentLocale, updateLocale } = this.props;
+        const { localeList } = this.state;
         return (
-            <select>
-                <option value="en">English</option>
-                <option value="es">Spanish</option>
-                <option value="fr">French</option>
-                <option value="it">Italian</option>
-                <option value="ja">Japanese</option>
+            <select onChange={event => updateLocale(event.target.value)}>
+                {localeList.map(locale => {
+                    return (
+                        <option
+                            key={locale.name}
+                            value={locale.tag}
+                            selected={currentLocale === locale.tag}
+                        >
+                            {locale.name}
+                        </option>
+                    );
+                })}
             </select>
         );
     }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(SelectLocale); 
+export default withRouter(
+    connect(mapStateToProps, mapDispatchToProps)(SelectLocale)
+);
